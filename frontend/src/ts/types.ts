3aff41664613d039306a11bfd25d0fed7b1fd59a
FruitPunchSamurai <spatel752@gatech.edu>
2023-11-15 20:28:28 -0500

    Refactor types,interface, and enums to separate directory and add error handling and custom authConstants

--- frontend/src/ts/types.ts
+++ frontend/src/ts/types.ts
@@ -1,4 +1,26 @@
 import {store} from "../redux/store";
+import {ReactElement} from "react";
+import {ToastPosition} from "@chakra-ui/react";
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+export type ToastStatusOptions =  "info" | "warning" | "success" | "error" | "loading" | undefined;
+
+
+export type User = {
+    Id: number,
+    Name: string,
+    Email: string,
+    CreatedAt: Date
+}
+
+export type AuthenticationToken = {
+    token: string,
+    expiry: Date
+}
+
+export type LinkData = {
+    name: String;
+    path: String;
+    component: () => ReactElement;
+}
--- frontend/src/utils/authConstants.ts
+++ frontend/src/utils/authConstants.ts
@@ -1,4 +1,9 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const DEVELOPMENT_API_BASE_URL = 'http://localhost:4000/v1';
+
+export const PRODUCTION_API_BASE_URL = '';
+
+
+export const apiUrl = process.env.NODE_ENV === "development" ? DEVELOPMENT_API_BASE_URL : PRODUCTION_API_BASE_URL;
 
 export const AV_API_KEY = "2H26FC7FC12UM05I";
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
