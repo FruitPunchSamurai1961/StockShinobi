fbc8f85517501549f2040ed5e0f56c33d06b134a
FruitPunchSamurai <spatel752@gatech.edu>
2023-11-25 02:26:43 -0500

    Add Stock List Call to RTK Query

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -5,6 +5,7 @@ import {authApi} from "./api/authApi";
 import {loginReducer} from "./login/loginSlice";
 import {rtkQueryErrorLogger} from "./middleware";
 import {contextReducer} from "./context/contextSlice";
+import {stockApi} from "./api/stockApi";
 
 export const store = configureStore({
     reducer: {
@@ -12,10 +13,12 @@ export const store = configureStore({
         login: loginReducer,
         user: userReducer,
         context: contextReducer,
-        [authApi.reducerPath]: authApi.reducer
+        [authApi.reducerPath]: authApi.reducer,
+        [stockApi.reducerPath]: stockApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
             .concat(authApi.middleware)
+            .concat(stockApi.middleware)
             .concat(rtkQueryErrorLogger)
 });
