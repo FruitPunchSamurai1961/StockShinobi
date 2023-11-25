ffd1112d41ce5c70f7d87f13d90522c1708aeb54
FruitPunchSamurai <spatel752@gatech.edu>
2023-11-25 02:28:40 -0500

    Refactor code
    Added a overall container in Root for all pages to be rendered.
    
    Move navbar to navbars
    
    Moved react,strict mode to be above App
    
    Delete Home.css
    
    Refined NotFound.tsx

--- frontend/src/components/routes/Root.tsx
+++ frontend/src/components/routes/Root.tsx
@@ -1,12 +1,16 @@
 import {Outlet} from "react-router-dom";
-import Navbar from "../navbar/Navbar";
+import Navbar from "../navbars/Navbar";
+import {Container} from "@chakra-ui/react";
 
 const Root = () => {
     return (
         <>
             <Navbar/>
-            <Outlet/>
-        </>);
+            <Container minW='100%' maxW='100%' minH='100%' maxH='100%' >
+                <Outlet/>
+            </Container>
+        </>
+    );
 }
 
 export default Root;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(
 
 
 root.render(
-    <App/>
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
