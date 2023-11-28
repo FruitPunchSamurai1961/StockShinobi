d33db7ea96b095b50076de5611a6860849d48fd5
FruitPunchSamurai <spatel752@gatech.edu>
2023-11-28 03:23:08 -0500

    Added endpoint in frontend to take in daily adjusted values from backend. Used a custom parser since this was dynamic json

--- frontend/src/redux/api/stockApi.ts
+++ frontend/src/redux/api/stockApi.ts
@@ -1,13 +1,65 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {apiUrl} from "../../utils/authConstants";
-import {RootState} from "../../ts/types";
+import {
+    AdjustedTimeSeriesStock,
+    DailyAdjustedData,
+    MetaData,
+    DailyAdjustedTimeSeries,
+    RootState
+} from "../../ts/types";
 import {requestMethod} from "../../ts/enums";
-import {ActiveListingResponse, StocksNewsResponse, TopStocksResponse} from "../../ts/interfaces";
+import {ActiveListingResponse, DailyAdjustedResponse, StocksNewsResponse, TopStocksResponse} from "../../ts/interfaces";
 import queryString from 'query-string';
+
+
+const parseDailyAdjustedResponse = (response: any): DailyAdjustedResponse => {
+    const parsedDailyAdjustedData = response["dailyAdjustedData"];
+    const parsedMetaData = parsedDailyAdjustedData["Meta Data"];
+    const parsedDailyAdjustedTimeSeries = parsedDailyAdjustedData["Time Series (Daily)"];
+
+    const metaData: MetaData = {
+        symbol: parsedMetaData["1. Information"],
+        information: parsedMetaData["2. Symbol"],
+        lastRefreshed: parsedMetaData["3. Last Refreshed"]
+    }
+
+
+    let adjustedTimeSeriesStockArray: AdjustedTimeSeriesStock[] = [];
+
+    for (const date of Object.keys(parsedDailyAdjustedTimeSeries)) {
+        const stockData = parsedDailyAdjustedTimeSeries[date];
+        adjustedTimeSeriesStockArray.push({
+            date: date,
+            open: Number(stockData["1. open"]),
+            high: Number(stockData["2. high"]),
+            low: Number(stockData["3. low"]),
+            close: Number(stockData["4. close"]),
+            adjustedClose: Number(stockData["5. adjusted close"]),
+            volume: Number(stockData["6. volume"]),
+            dividendAmount: Number(stockData["7. dividend amount"])
+        });
+    }
+
+
+    let dailyAdjustedTimeSeries: DailyAdjustedTimeSeries = {
+        adjustedTimeSeriesStockArray: adjustedTimeSeriesStockArray
+    }
+
+
+    const dailyAdjustedData: DailyAdjustedData = {
+        metaData: metaData,
+        dailyAdjustedTimeSeries: dailyAdjustedTimeSeries
+    }
+
+    return {
+        dailyAdjustedData: dailyAdjustedData
+    }
+}
+
 
 export const stockApi = createApi({
     reducerPath: 'stockApi',
-    keepUnusedDataFor: 500,
+    keepUnusedDataFor: 5000,
     baseQuery: fetchBaseQuery({
         baseUrl: `${apiUrl}/stocks`,
         prepareHeaders: (headers: Headers, {getState}) => {
@@ -37,8 +89,28 @@ export const stockApi = createApi({
                 method: requestMethod.GET
             }),
         }),
+        getDailyAdjustedDataForStock: builder.query<DailyAdjustedResponse | undefined, { symbol: string }>({
+            query: ({symbol}) => ({
+                url: `/timeseries/daily-adjusted`,
+                method: requestMethod.GET,
+                params: {
+                    symbol: symbol
+                },
+            }),
+            transformResponse: (response: string): DailyAdjustedResponse | undefined => {
+                if (response == null) {
+                    return undefined
+                }
+                return parseDailyAdjustedResponse(response)
+            },
+        }),
     })
 });
 
 
-export const {useGetActiveStocksListQuery, useGetNewsForStocksQuery, useGetTopGainersLosersStockListQuery} = stockApi;
+export const {
+    useGetActiveStocksListQuery,
+    useGetNewsForStocksQuery,
+    useGetTopGainersLosersStockListQuery,
+    useGetDailyAdjustedDataForStockQuery
+} = stockApi;
